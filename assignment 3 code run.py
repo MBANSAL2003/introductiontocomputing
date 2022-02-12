286c3bf731a84bb07d989108ae7981090317669f
MBANSAL2003 <96760473+MBANSAL2003@users.noreply.github.com>
2022-02-12 21:35:15 +0530

    Add files via upload

---
+++ assignment 3 code run.py
@@ -0,0 +1,68 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:\Users\Dell\Desktop\aa.py =====================
+Please enter your own String : my name is mehul bansal
+Please enter your own Character : m
+The total Number of Times  m  has Occurred =  3
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+ WRITE THE YEAR:2022
+WRITE A MONTH[1-12]:6
+WRITE THE DAY[1-31]:8
+THE NEXT DATE IS [YYYY/MM/DD] 2022-6-9.
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+The list is
+[3, 9, 10]
+The resultant tuple is :
+[(3, 9), (9, 81), (10, 100)]
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+enter grade of student:
+8
+your grade is 'B+' and very good performance
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+Enter your sid: 
+21104099
+Enter name: 
+mehul bansal
+Do you want to enter another student entry(Y or N):y
+Enter sid: 21104098
+Enter name: q
+Do you want to enter another student entry(Y or N):n
+<a>
+ {21104099: 'mehul bansal', 21104098: 'q'}
+<b>
+ {21104099: 'mehul bansal', 21104098: 'q'}
+<c>
+ {21104098: 'q', 21104099: 'mehul bansal'}
+Search Name with SID: 
+21104098
+<d>
+ q
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+ENTER THE NUMBER OF TERMS IN THE SERIES:  
+5
+Fibonacci sequenceL: 
+0
+1
+1
+2
+3
+AVERAGE OF THE RESULTANT FIBONACCI SERIES IS:  1.4
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+<a>
+ {1, 3, 5, 6, 8}
+<b>
+ {3, 6, 8, 9, 13, 17}
+<c>
+ {1, 2, 4, 5}
+<d> 
+ {6, 7, 8, 9, 10}
+<e>
+ {10}
+-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
